9243be58c612c3d3a35d2cbd4d54b04a5998b50e
NEO7171 <dgedai2007@yandex.ru>
2022-07-28 12:23:32 +0300

    Миграции в Laravel урок 10

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,8 @@ class HomeController extends Controller
 {
     public function index()
     {
+        dump($_ENV['DB_DATABASE']);
+        dump($_ENV);
         return view('home', ['res'=> 5, 'name'=>'John']);
     }
     public function test()
