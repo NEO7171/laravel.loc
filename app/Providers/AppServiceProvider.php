c982fd6517224efd095f7708882dcf605a88ce91
NEO7171 <dgedai2007@yandex.ru>
2022-08-16 23:57:52 +0300

    View Composers урок 32

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
+use App\Rubric;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -28,5 +29,9 @@ class AppServiceProvider extends ServiceProvider
         DB::listen(function ($query){
             //dump($query->sql);
         });
+        view()->composer('layouts.footer', function ($view){
+            $view->with('rubrics', Rubric::all());
+        });
+
     }
 }
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,6 +4,11 @@
             <a href="#">Back to top</a>
         </p>
         <p>&copy{{date('Y')}}</p>
+        <ul class="d-flex">
+            @foreach($rubrics as $rubric)
+                <li class="mr-3"><a href="#">{{$rubric->title}}</a></li>
+            @endforeach
+        </ul>
         <p>New to Bootstrap? <a href="/">Visit the homepage</a> or read our <a
                 href="/docs/4.6/getting-started/introduction/">getting started guide</a>.</p>
     </div>
